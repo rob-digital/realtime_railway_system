52a5afe34e215c4432736c73506e59e2ef4c717a
Robert Roksela <Robert.Roksela@computershare.co.uk>
2023-04-13 19:04:57 +0100

    initial commit

---
+++ src/main/java/rob/digital/realtime_railway_system/RealtimeRailwaySystemApplication.java
@@ -0,0 +1,13 @@
+package rob.digital.realtime_railway_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealtimeRailwaySystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RealtimeRailwaySystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/rob/digital/realtime_railway_system/RealtimeRailwaySystemApplicationTests.java
@@ -0,0 +1,13 @@
+package rob.digital.realtime_railway_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealtimeRailwaySystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
